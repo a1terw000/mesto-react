1a2ddfff8693d29fc01f8d9aa8ac4fb0a2861b1f
Данил Байдак <lazya142425@gmail.com>
2023-07-10 17:35:15 +0300

    ПР10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header/Header.jsx";
 import Main from "./Main/Main.jsx";
 import Footer from "./Footer/Footer.jsx";
 import PopupWithForm from "./PopupWithForm/PopupWithForm.jsx";
-import PopupImage from "./PopupImage/PopupImage.jsx";
+import PopupImage from "./ImagePopup/PopupImage.jsx";
 import {useState} from "react";
 
 function App() {
